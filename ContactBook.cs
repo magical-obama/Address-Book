4f0276c3b2dbf10b403a714d32b974d14f4395e7
magical-obama <maschwaerzler@a1.net>
2022-04-15 21:06:05 +0200

    Changing ContactBook from List to BindingList

--- ContactBook.cs
+++ ContactBook.cs
@@ -1,44 +1,72 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.ComponentModel;
 using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
 
 namespace Address_Book
 {
-    public class ContactBook : List<Contact>
+    public class ContactBook : BindingList<Contact>, INotifyPropertyChanged
     {
+        public ContactBook() { }
+
         public ContactBook(List<Contact> init)
         {
-            this.AddRange(init);
+            foreach (var contact in init)
+            {
+                this.Add(contact);
+                UpdateChanges();
+            }
         }
-        
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void UpdateChanges() {
+            NotifyPropertyChanged();
+            this.ResetBindings();
+        }
+
         public Contact? Find(string id)
         {
-            return this.Find(c => c.Id == id);
+            return this.SingleOrDefault(contact => contact.Id == id);
         }
 
         public bool Remove(string id)
         {
-            var contact = this.Find(id);
+            Contact? contact = this.Find(id);
             if (contact != null)
             {
                 return this.Remove(contact);
+                UpdateChanges();
             }
             return false;
         }
 
-        public bool UpdateContact(string id, Contact newContact)
+        public bool UpdateContact(string id, string name, string address, string telephoneNumber)
         {
-            var oldContact = this.Find(id);
-            if (oldContact != null)
+            Contact? contact = this.Find(id);
+            if (contact != null)
             {
-                oldContact.Name = newContact.Name;
-                oldContact.address = newContact.address;
-                oldContact.telephoneNumber = newContact.telephoneNumber;
+                contact.Update(name, address, telephoneNumber);
+                System.Diagnostics.Debug.WriteLine("Contact updated");
+                UpdateChanges();
                 return true;
             }
             return false;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            foreach (var contact in this)
+            {
+                sb.AppendLine(contact.ToString());
+                sb.AppendLine();
+            }
+            return sb.ToString();
         }
     }
 }
