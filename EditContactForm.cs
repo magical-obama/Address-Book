e655922a64931b400f9aca02b4bdebf724dadf27
magical-obama <maschwaerzler@a1.net>
2022-04-15 21:34:08 +0200

    Cleanup

--- EditContactForm.cs
+++ EditContactForm.cs
@@ -1,14 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace Address_Book
+﻿namespace Address_Book
 {
     public partial class EditContactForm : Form
     {
